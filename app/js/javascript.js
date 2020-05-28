e08f349e36b54522e81c101b6b375b854760774a
TARA ENGELHARDT <taraengy@gmail.com>
2020-05-28 12:04:50 +0100

    sidebar collapsable but was pointless as i already have tabs

---
+++ app/js/javascript.js
@@ -0,0 +1,9 @@
+ // <script>
+        $(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});
+// </script>    
